6cb3787f194c5548f583f8a1efbecb82173d44fa
xclusive2010 <ongurd5@gmail.com>
2023-06-04 19:27:29 +0100

    uuu

---
+++ src/components/homeSection2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <img src="./icons/image-3.png" alt="">
+    </div>
+</template>
+
+<script>
+export default{
+   
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
