529123011a8c8e2642612a988a00ee81fbb3660f
xclusive2010 <ongurd5@gmail.com>
2023-06-04 13:59:12 +0100

    done

---
+++ src/components/navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        
+      <img src="./icons/logo-light (1).png" alt="">
+
+      <ul>
+        <li>
+            <a href="">Home </a>
+            <a href="">About</a>
+            <a href="">Project</a>
+            <a href="">Services</a>
+            <a href="">Pages</a>
+            <a href="">Contact</a>
+        </li>
+      </ul>
+    
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style  scoped>
+div{
+    
+    padding-top: 1rem;
+    padding-right: 2rem;
+    padding-left: 2rem;
+};
+
+li{
+    list-style: none;
+     margin-left: 2rem
+};
+a{
+    margin-left: 1rem;
+    text-decoration: none;
+};
+ul{
+    list-style: none;
+};
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script>
+
+import navbar from '../components/navbar.vue';
+export default{
+  components:{
+    navbar
+  }
+}
+</script>
+
+<template>
+  <main>
+    <header>
+    <navbar/>
+  </header>
+  </main>
+</template>
+<style scoped>
+header{
+    background-image: url('../assets/banner-1.jpg');
+    background-size: cover;
+    height: 95vh;
+}
+
+</style>
