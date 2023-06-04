7d81d7c43bf1b74adb6f844eb6fabef2b7899ab0
xclusive2010 <ongurd5@gmail.com>
2023-06-04 17:10:39 +0100

    fish

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-        
-      <img src="./icons/logo-light (1).png" alt="">
 
-      <ul>
-        <li>
-            <a href="">Home </a>
-            <a href="">About</a>
-            <a href="">Project</a>
-            <a href="">Services</a>
-            <a href="">Pages</a>
-            <a href="">Contact</a>
-        </li>
-      </ul>
-    
+        <img src="./icons/logo-light (1).png" alt="">
+
+        <ul>
+            <li>
+                <a href="/">Home </a>
+                <a href="/about">About</a>
+                <a href="">Project</a>
+                <a href="">Services</a>
+                <a href="">Pages</a>
+                <a href="">Contact</a>
+            </li>
+        </ul>
+
     </div>
 </template>
 
@@ -21,24 +21,34 @@
 
 </script>
 
-<style  scoped>
-div{
-    
-    padding-top: 1rem;
-    padding-right: 2rem;
-    padding-left: 2rem;
-};
-
+<style lang="scss" scoped>
+div {
+ display: flex;
+ justify-content: space-between;
+ padding-top: 2rem;
+ text-align: center;
+ padding-left: 4rem;
+ padding-right: 4rem;
+ 
+ ul{
+  list-style: none;
+  margin-top: 18px;  
+ }
 li{
-    list-style: none;
-     margin-left: 2rem
-};
+  text-decoration: none,  
+}
 a{
-    margin-left: 1rem;
-    text-decoration: none;
-};
-ul{
-    list-style: none;
-};
+    text-decoration:none;
+    margin-left: 1.3rem;
+    font-size: 1.3rem;
+    font-weight:2.5rem;
+    color: white;
+    font-family: Roboto;
+    
+}   
+  
 
+}
+
+;
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,14 +12,51 @@ export default{
   <main>
     <header>
     <navbar/>
+    <img src="../assets/image-2.png" alt="" class="house">
+    <img src="../assets/image-1.png" alt="" class="man">
+    <div class="text">
+      <h2>VISION IN </h2>
+    <h1>BUIL<span>DING</span> </h1>
+
+    </div>
+    
   </header>
   </main>
 </template>
-<style scoped>
+<style lang="scss" scoped>
 header{
     background-image: url('../assets/banner-1.jpg');
     background-size: cover;
-    height: 95vh;
+    height: 100vh;
+    font-family: Roboto, sans-serif;
+    color:white;
+
+    
+.house{
+  
+  margin-left:51.5rem;
+  margin-top: 10.2rem;
+}
+.man{
+  margin-left: 40rem;
+  margin-top: -59rem;
+  height: 40rem;
+}
+.text{
+  margin-top: -28rem;
+  margin-left: 10rem;
+  h2{
+    font-size: 4rem;
+  }
+  h1{
+    font-size:10rem;
+    span{
+    -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: rgb(255 255 255 / 60%);
+    color: transparent
+    }
+  }
+}
 }
 
 </style>
