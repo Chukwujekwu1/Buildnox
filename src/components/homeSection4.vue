7ada06559ae258bee7e170f170b3d634ef6ed72c
xclusive2010 <ongurd5@gmail.com>
2023-06-07 04:52:47 +0100

    that all for to day

--- src/components/homeSection4.vue
+++ src/components/homeSection4.vue
@@ -9,21 +9,49 @@
                 <h3>{{ h3 }}</h3>
             </div>
             <h1 class="text-5xl  font-bold mt-12" >{{ our }}</h1>
-            <p class="text-xl font-normal mt-12">{{ bring }}</p>
+            <p class="text-xl font-normal mt-12 mb-12">{{ bring }} </p>
+            <div v-for="word in words" class="flex -ml-2 mt-5" >
+                <img :src="word.img" alt="" class="w-6 h-6">
+                <h1 class='ml-4 text-xl font-medium'>{{ word.text }}</h1>
+            </div>
+            
+
         </div>
     </div>
 </template>
 
 <script >
 import bgImage from './icons/bg-1.jpg'
+import check from './icons/check-circle.png'
 export default {
     data() {
         return {
             bgImage,
+            check,
             h3: 'OUR KEY POINTS',
             our: 'Why Choose Us',
-            bring:'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation . Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward.'
+            bring:'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation . Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward.',
+            words:
+            [
+              {
+                text:'Experienced Construction Professional',
+                img: check
+              },
+              {
+                text:'Committted to Helping Our Clients',
+                img: check
+              },
+              {
+                text:'Building Maintenance & Renovation',
+                img:check
+              },
+              {
+                text:'Innovative Eco Power Technologies',
+                img: check
+              }
+            ]
         }
+
     }
 }
 </script>
