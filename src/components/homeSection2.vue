562f65e2e825f1f1877cedd1283092d203e2291f
xclusive2010 <ongurd5@gmail.com>
2023-06-08 20:48:27 +0100

    section5

--- src/components/homeSection2.vue
+++ src/components/homeSection2.vue
@@ -1,50 +1,50 @@
 <template>
-  <div>
+  <div class="p-20 pt-0 div flex-row flex justify-between">
 
-    <img :src="walk" alt="" class="walk" >
+    <img :src="walk" alt="" class="walk">
 
-    <div class="htext flex-col">
+    <div class=" flex-col  p-20 pr-0">
 
       <div class="hr3">
         <hr>
-        <h3 >{{ h3 }}</h3>
+        <h3>{{ h3 }}</h3>
       </div>
 
       <h1 class="con">
-        {{ con }}<span class="bus">{{ bus }}</span>
+        {{ con }}<span class="bus ml-4"> {{ bus }} </span>
       </h1>
 
       <p class="we">
-       {{ we }}
+        {{ we }}
       </p>
 
       <p class="for">
-       {{ for }}
+        {{ for }}
       </p>
 
       <div class="fS">
 
         <div class="foundee">
 
-          <img :src="author" alt="">
+          <img :src="author" alt="" class="author">
 
-           <div class="ro">
+          <div class="ro">
 
             <h2>{{ rob }}</h2>
             <p>{{ founder }}</p>
 
-           </div>
+          </div>
 
         </div>
 
-        <div>
-            <img :src="signature" alt="">
+        <div class="ml-32">
+          <img :src="signature" alt="">
         </div>
 
       </div>
 
     </div>
-    <p class="bottom"> 
+    <p class="bottom">
 
     </p>
 
@@ -52,22 +52,22 @@
 </template>
 
 <script>
-  import walk from './icons/image-3.png';
-  import author from './icons/author-1.jpg';
-  import signature from './icons/signature.png';
+import walk from './icons/image-3.png';
+import author from './icons/author-1.jpg';
+import signature from './icons/signature.png';
 export default {
-  data(){
-    return{
-      walk ,
+  data() {
+    return {
+      walk,
       author,
       signature,
-     h3:"WELCOME TO COMPANY",
-     con:'Construction Company With Strategy In',
-     bus: 'Business Growth',
-     we:' We have been the trusted name in heating, air conditioning and plumbing businesses. Throughout the years, our number one goal',     
-     for:' For over 80 years, We have been the trusted name in heating, air conditioning and plumbing businesses. Throughout the years, our number one goal has been to accurately . For over 80 years, We have been the trusted name in heating, air conditioning and plumbing businesses. Throughout the years, our number one goal has been to accurately .For over 80 years, We have been the trusted name in heating.',
-    rob:'Robert Joe Kerry',
-    founder: 'Founder',
+      h3: "WELCOME TO COMPANY",
+      con: 'Construction Company With Strategy In',
+      bus: 'Business Growth',
+      we: ' We have been the trusted name in heating, air conditioning and plumbing businesses. Throughout the years, our number one goal',
+      for: ' For over 80 years, We have been the trusted name in heating, air conditioning and plumbing businesses. Throughout the years, our number one goal has been to accurately . For over 80 years, We have been the trusted name in heating, air conditioning and plumbing businesses. Throughout the years, our number one goal has been to accurately .For over 80 years, We have been the trusted name in heating.',
+      rob: 'Robert Joe Kerry',
+      founder: 'Founder',
     }
   },
 
@@ -75,97 +75,95 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-div {
+<style lang="css" scoped>
+.div {
 
   background-color: #13203b;
-  padding-left: 3.5rem;
-  padding-right: 3.5rem;
+  
   font-family: Roboto, sans-serif;
   color: white;
 
+}
 
-  .walk {
-    height: 80%;
-  }
+.walk {
+  height: 80%;
+}
 
-  .htext {
 
-    margin-left: 15rem;
 
-    .hr3 {
-      display: flex;
-      margin-top: -40rem;
+.hr3 {
+  display: flex;
+  margin-top: 2rem;
+}
 
-      hr {
-        width: 2.5rem;
-        height: 2px;
-        margin-top: 10px;
-        background-color: orangered;
-        border: none;
-      }
+hr {
+  width: 2.5rem;
+  height: 2px;
+  margin-top: 10px;
+  background-color: orangered;
+  border: none;
+}
 
-      h3 {
-        color: orangered;
-        margin-left: 1rem;
-        font-size: 1rem;
-    font-weight: 550;
-
-      }
-    }
-
-    .con {
-      margin-left: 4rem;
-      font-size: 2.6rem;
-      font-weight: 750;
-      margin-top: 3rem;
-
-      .bus {
-        font-weight: 20;
-        font-size: 2rem;
-      }
-    }
-
-    .we {
-      margin-left: 4rem;
-      font-size: 1.5rem;
-      margin-top: 3rem;
-      line-height: 2.5rem;
-    }
-
-    .for {
-      margin-left: 4rem;
-      font-size: 1.2rem;
-      margin-top: 2rem;
-      line-height: 2rem;
-    }
-
-    .fS {
-      margin-left: -2.8rem;
-      margin-top: 1.7rem;
-      display: flex;
-
-      .foundee {
-        display: flex;
-
-        img {
-          border-radius: 360%;
-        }
-
-        .ro {
-          margin-top: 1rem;
-          
-
-          p {
-            margin-top: 10px;
-          }
-        }
-      }
-    }
-  }
-  .bottom{
-    height:7rem;
-  }
+h3 {
+  color: orangered;
+  margin-left: 1rem;
+  font-size: 1rem;
+  font-weight: 550;
 
 }
-</style>
+
+
+.con {
+ 
+  font-size: 2.6rem;
+  font-weight: 750;
+  margin-top: 3rem;
+}
+
+.bus {
+  font-weight: 20;
+  font-size: 2rem;
+
+}
+
+.we {
+  
+  font-size: 1.5rem;
+  margin-top: 3rem;
+  line-height: 2.5rem;
+}
+
+.for {
+ 
+  font-size: 1.2rem;
+  margin-top: 2rem;
+  line-height: 2rem;
+}
+
+.fS {
+  
+  margin-top: 1.7rem;
+  display: flex;
+  
+}
+
+.foundee {
+  display: flex;
+}
+
+.author {
+  border-radius: 360%;
+}
+
+.ro {
+  margin-top: 1rem;
+  margin-left: 1rem;
+}
+
+.bottom {
+  height: 7rem;
+}
+
+p {
+  margin-top: 10px;
+}</style>
--- src/components/homeSection5.vue
+++ src/components/homeSection5.vue
@@ -1,40 +1,75 @@
 <template>
     <div class="div">
-       <div class="h-96 blue">
-        <div class="flex-col">
-            <div class="flex-row">
-                <hr>
-                <h3>{{ header1 }}</h3>
+        <div class="blue flex-row flex justify-between p-20 ">
+            <div class="flex-col">
+                <div class="flex-row flex">
+                    <hr>
+                    <h3>{{ header1 }}</h3>
+                </div>
+                <h1 class="text-5xl  font-bold  text-white mt-8">{{ header2 }}</h1>
             </div>
+            <button @click.prevent=""
+                class=" w-72 h-20 bg-red-500 text-center anim  mt-8 animate__animated animate__fadeInUpBig"> {{ btntext
+                }}</button>
         </div>
-       </div>
-
+        <div class="flex-row flex justify-between p-6 -mt-72 pt-0">
+            <img :src="roofImg" alt="" class="rounded-md">
+            <img :src="buildImg" alt="" class="rounded-md">
+            <img :src="generalContructionImg" alt="" class="rounded-md">
+        </div>
+        <swiper :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange" class="pad">
+            <swiper-slide class="text1">{{ text1 }}</swiper-slide>
+            <swiper-slide class="text1">{{ text1 }}</swiper-slide>
+            ...
+        </swiper>
     </div>
 </template>
 
 <script >
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import roofImg from '../components/icons/gallery-1.jpg';
-import consructionImg from '../components/icons/gallery-2.jpg';
+import buildImg from '../components/icons/gallery-2.jpg';
 import generalContructionImg from '../components/icons/gallery-3.jpg';
-export default{
-    data (){
-        return{
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    data() {
+        return {
             roofImg,
-            consructionImg,
+            buildImg,
             generalContructionImg,
-            header1:'OUR WORKS'
-
+            text1: 'TECHNOLOGY CIVIL',
+            header1: 'OUR WORKS',
+            header2: 'Explore Recent Projects',
+            btntext: 'VIEW ALL PROJECTS',
         }
     }
 }
 </script>
 
 <style lang="css" scoped>
-.div{
+.div {
     margin-top: -0.7rem;
 }
-.blue{
- background-color: #13203b;
+
+.text1 {
+    -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: rgba(173, 167, 167, 0.6);
+    color: transparent;
+    font-size: 9rem;
+}
+
+img {
+    width: 26.3rem;
+    height: 25rem;
+}
+
+.blue {
+    height: 33rem;
+    background-color: #13203b;
 }
 
 h3 {
@@ -53,4 +88,25 @@ hr {
     background-color: orangered;
     border: none;
 }
+
+.anim {
+    font-family: Roboto, sans-serif;
+    font-size: 1rem;
+    font-weight: 550;
+    transition: 0.9s;
+    border-radius: 8px;
+
+}
+
+.anim:hover {
+    background-color: rgb(11, 11, 39);
+    color: white
+}
+
+.pad {
+    padding: 2.5rem;
+    padding-top: 0rem;
+    margin-top: -2rem;
+}
+
 </style>
